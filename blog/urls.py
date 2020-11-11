a5967161fa1d5664bec8dfaf400e38a936f692ef
Kenshin-Shibata <ps0139hk@gmail.com>
2020-11-12 07:46:11 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views #blogアプリの全てのビューをインポート
 urlpatterns = [
     #最初のURLパターンを追加する
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
